a2e80c0a1b857c34421b97bcab4c1f8a3ca415ae
alyabozied <42724850+alyabozied@users.noreply.github.com>
2019-11-28 21:40:07 +0200

    merging game

---
+++ Game.asm
@@ -0,0 +1,137 @@
+include p1m.inc 
+include barrier.inc
+.model small
+.stack
+.data
+plen   equ  20      ; height and width of player
+p1x    dw   0      ; left upper cornder 
+p1y    dw   0   
+m1x    dw   0       ; right bottom corner 
+m1y    dw   0  
+p1cl   db   09h     ; p1 body color
+p1cd   db   01h     ; p1 link color
+Pipx dw 0 
+seed dw 0
+Gap dw 0 ; y of the gap in pipe
+Running db  0h   
+Tunnel dw   0h  
+TunnelSize dw 24
+.code
+main proc far 
+    mov ax,@data
+    mov ds,ax 
+    ; change graphics mode
+    mov ah,0
+    mov al,13h
+    int 10h
+    mov Pipx ,100
+	getrandom Gap seed
+    mov Running, 1
+GameLoop:
+	
+    ; Clear
+    Call Clear
+		dec Pipx
+    cmp Pipx,-1
+	jnz nowrap
+		mov Pipx,100
+		getrandom Gap seed
+	nowrap:
+	; Get Input
+    Call GetInput   
+    ; Draw
+    Call Draw
+    ; Delay
+    Call Delay
+	mov ax ,Pipx
+	cmp ax,20
+	jnz complete
+    mov ax, p1y
+	sub ax,Gap
+	cmp ax,1
+	jz complete
+		mov Running ,0
+	complete:
+    cmp Running, 1                                  
+    je GameLoop 
+    
+    
+	mov ah, 4ch
+	int 21h
+	
+main endp
+
+
+;------Clear Screen----- 
+Clear proc
+    ClearP1 p1x, p1y, plen, m1x , m1y ; Clear Player1
+	DeletePipe Pipx ,Gap
+    ret
+Clear endp 
+;-----------------------
+    
+;------Get Input----- 
+GetInput proc
+    mov ah, 1
+    int 16h   ; Get Key Pressed
+    jz Return ; If no Key Pressed Return
+         
+    ; Key Pressed
+    cmp ah, 48h     ; IF UP ARROW MOVE UP
+    je MoveUp
+   
+    cmp ah, 50h     ; IF DOWN ARROW MOVE DOWN
+    je MoveDown
+   
+    mov Running, 0  ; Else Exit 
+    jmp Return
+    ;------------
+
+MoveUp:
+    cmp Tunnel, 0   
+    je Return
+    DEC Tunnel  
+    jmp Update
+
+MoveDown:
+    cmp Tunnel, 6
+    je Return
+    INC Tunnel  
+      
+Update:
+    mov ax, Tunnel
+    mul TunnelSize
+    mov p1y, ax     
+    ADD p1y, 0
+    
+Return:
+    ; Flush Keyboard Buffer
+    mov ah,0ch
+    mov al,0
+    int 21h
+    ret
+GetInput endp 
+;-----------------------   
+    
+;------Draw Function---- 
+Draw proc  
+    ; Draw Player 1
+    
+    DrawPipe Pipx , Gap  
+	DrawP1 p1x, p1y, plen, m1x , m1y , p1cl , p1cd
+    ret
+
+Draw endp
+;-----------------------
+                           
+;------Delay Function---- 
+Delay proc  
+    mov ah, 86h
+    mov cx, 1h
+    mov dx, 86A0h
+    int 15h
+    ret
+
+Delay endp
+;----------------------- 
+end main
